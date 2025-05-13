387d8349547b14bc54517d53bf22c52ed21cd96b
Web Development <web@btech.edu>
2025-05-13 13:53:18 -0600

    enable creation of testimonials

---
+++ util/generate-testimonial.js
@@ -0,0 +1,7 @@
+import { faker } from "@faker-js/faker";
+export default function generateTestimonial() {
+	return {
+		feedback: faker.lorem.sentence(),
+		rating: faker.number.int({ min: 0, max: 5 }),
+	};
+}
